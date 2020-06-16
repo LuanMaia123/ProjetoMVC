bb384a8f9b7e11cd9968c1d9dae3ccba225a7fa0
Luan Maia <luan.maia@amcom.com.br>
2020-06-16 12:55:46 -0300

    Rodado migrations

---
+++ Dados/Migrations/20200616155359_InitialCreate.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dados.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Empresas",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Uf = table.Column<int>(nullable: false),
+                    varchar250 = table.Column<string>(name: "varchar(250)", nullable: true),
+                    varchar14 = table.Column<string>(name: "varchar(14)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empresas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: true),
+                    varchar7 = table.Column<string>(name: "varchar(7)", nullable: true),
+                    varchar250 = table.Column<string>(name: "varchar(250)", nullable: false),
+                    varchar11 = table.Column<string>(name: "varchar(11)", nullable: true),
+                    varchar14 = table.Column<string>(name: "varchar(14)", nullable: true),
+                    IsPessoaJuridica = table.Column<bool>(nullable: false),
+                    EmpresaId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fornecedores_Empresas_EmpresaId",
+                        column: x => x.EmpresaId,
+                        principalTable: "Empresas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Telefones",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    varchar12 = table.Column<string>(name: "varchar(12)", nullable: true),
+                    FornecedorId = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Telefones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Telefones_Fornecedores_FornecedorId",
+                        column: x => x.FornecedorId,
+                        principalTable: "Fornecedores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Empresas",
+                columns: new[] { "Id", "varchar(14)", "varchar(250)", "Uf" },
+                values: new object[] { 1L, "12.222.222/1111-11", "Pague Veloz", 23 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fornecedores_EmpresaId",
+                table: "Fornecedores",
+                column: "EmpresaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Telefones_FornecedorId",
+                table: "Telefones",
+                column: "FornecedorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Telefones");
+
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+
+            migrationBuilder.DropTable(
+                name: "Empresas");
+        }
+    }
+}
